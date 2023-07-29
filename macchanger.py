ed50f896444f8213a3d489eaeb6f71bc8a8cf8d6
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-07-28 22:22:03 -0700

    Code cleanup

--- macchanger.py
+++ macchanger.py
@@ -2,6 +2,25 @@
 import optparse
 import subprocess
 
+def get_args():
+    # Create parser object
+    parser = optparse.OptionParser()
+
+    # Set arguments
+    parser.add_option("-i","--interface",dest="interface",help="Interface to change its MAC address")
+    parser.add_option("-m","--mac",dest="new_mac",help="New MAC address")
+
+    # Listen for arguments
+    (options, arguments) = parser.parse_args()
+
+    # Error handling
+    if not options.interface:
+        parser.error("[-] Please specify an interface to use. See --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC to set. See --help for more info.")
+
+    return options
+
 def change_mac(interface, new_mac):
     print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
@@ -9,16 +28,8 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface,"hw","ether", new_mac])
     subprocess.call(["ifconfig", interface,"up"])
 
-# Create parser object
-parser = optparse.OptionParser()
 
-# Set arguments
-parser.add_option("-i","--interface",dest="interface",help="Interface to change its MAC address")
-parser.add_option("-m","--mac",dest="new_mac",help="New MAC address")
-
-# Listen for arguments
-(options, arguments) = parser.parse_args()
-
-change_mac(options.interface,options.new_mac)
+args = get_args()
+change_mac(args.interface,args.new_mac)
 
 
